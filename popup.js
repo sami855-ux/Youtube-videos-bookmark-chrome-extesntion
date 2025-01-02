e376b24f5df451f9d92b3898681ac41bf91d4d42
sami855-ux <samitale56@outlook.com>
2025-01-02 11:25:28 +0500

    added two new functions that handles the content in the popup

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
 import { getActiveURL } from "./utils.js"
 
-const addNewBookmark = () => {}
+const addNewBookmark = (bookmarkElement, bookmark) => {
+  const bookmarkTitleElement = document.createElement("div")
+  const newBookmarkElement = document.createElement("div")
 
-const viewBookmarks = () => {}
+  bookmarkTitleElement.textContent = bookmark.desc
+  bookmarkTitleElement.className = "bookmark-title"
+
+  newBookmarkElement.id = "bookmark-" + bookmark.time
+}
+
+const viewBookmarks = (currentVideoBookmark = []) => {
+  const bookmarkElement = document.getElementById("bookmarks")
+  bookmarkElement.innerHTML = ""
+
+  if (currentVideoBookmark.length > 0) {
+    for (let i = 0; i < currentVideoBookmark.length; i++) {
+      const bookmark = currentVideoBookmark[i]
+
+      addNewBookmark(bookmarkElement, bookmark)
+    }
+  } else {
+    bookmarkElement.innerHTML =
+      "<div class='title'>No bookmarks available</div>"
+  }
+}
 
 const onPlay = (e) => {}
 
@@ -22,6 +44,14 @@ document.addEventListener("DOMContentLoaded", async () => {
       const currentVideoBookmark = obj[currentVideo]
         ? JSON.parse(obj[currentVideo])
         : []
+
+      //viewBookmarks
+      viewBookmarks(currentVideoBookmark)
     })
+  } else {
+    //If the current tab is not a youtube video, show a message
+    const container = document.getElementsByName("container")[0]
+
+    container.innerHTML = "<div class='title'>This is not a youtube video</div>"
   }
 })
