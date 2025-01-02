9bb242ba177d2061c7b5424db4e2b55661058e7e
sami855-ux <samitale56@outlook.com>
2025-01-02 13:48:24 +0500

    Play button is added on the bookmark

--- popup.js
+++ popup.js
@@ -3,15 +3,21 @@ import { getActiveURL } from "./utils.js"
 const addNewBookmark = (bookmarkElement, bookmark) => {
   const bookmarkTitleElement = document.createElement("div")
   const newBookmarkElement = document.createElement("div")
+  const controlsElement = document.createElement("div")
 
   bookmarkTitleElement.textContent = bookmark.desc
   bookmarkTitleElement.className = "bookmark-title"
+
+  controlsElement.className = "bookmark-controls"
 
   newBookmarkElement.id = "bookmark-" + bookmark.time
   newBookmarkElement.className = "bookmark"
   newBookmarkElement.setAttribute("timestamp", bookmark.time)
 
+  setBookmarkAttributes("play", onPlay, controlsElement)
+
   newBookmarkElement.appendChild(bookmarkTitleElement)
+  newBookmarkElement.appendChild(controlsElement)
   bookmarkElement.appendChild(newBookmarkElement)
 }
 
@@ -35,7 +41,15 @@ const onPlay = (e) => {}
 
 const onDelete = (e) => {}
 
-const setBookmarkAttributes = () => {}
+const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
+  const controlElement = document.createElement("img")
+
+  controlElement.src = "assets/" + src + ".png"
+  controlElement.title = src
+  controlElement.addEventListener("click", eventListener)
+
+  controlParentElement.appendChild(controlElement)
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
   const activeTab = await getActiveURL()
