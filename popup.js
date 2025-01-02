7becd36a0f79a40136d869ada3ca5ea492b7c478
sami855-ux <samitale56@outlook.com>
2025-01-02 11:07:36 +0500

    Check the video when the DOM is loaded

--- popup.js
+++ popup.js
@@ -1,12 +1,27 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+import { getActiveURL } from "./utils.js"
 
-const viewBookmarks = () => {};
+const addNewBookmark = () => {}
 
-const onPlay = e => {};
+const viewBookmarks = () => {}
 
-const onDelete = e => {};
+const onPlay = (e) => {}
 
-const setBookmarkAttributes =  () => {};
+const onDelete = (e) => {}
 
-document.addEventListener("DOMContentLoaded", () => {});
+const setBookmarkAttributes = () => {}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getActiveURL()
+  const queryParameters = activeTab.url.split("?")[1]
+  const urlParameters = new URLSearchParams(queryParameters)
+
+  const currentVideo = urlParameters.get("v")
+
+  if (activeTab.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([currentVideo], (obj) => {
+      const currentVideoBookmark = obj[currentVideo]
+        ? JSON.parse(obj[currentVideo])
+        : []
+    })
+  }
+})
