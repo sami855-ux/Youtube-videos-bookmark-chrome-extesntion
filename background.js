d21820285526688bd8f54a20abbc3fea4814950d
sami855-ux <samitale56@outlook.com>
2025-01-02 09:53:48 +0500

    set the videos should be youtube videos and send the video id to the contentScript.js

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
 chromes.tabs.onUpdated.addListener((tabId, tab) => {
   //Every time a tab is updated, check if the url is a youtube video
   if (tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1]
+    const urlParams = new URLSearchParams(queryParameters) //Get the query parameters
+
+    // Send a message to the content script with the video id
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW_VIDEO",
+      videoId: urlParams.get("v"),
+    })
   }
 })
