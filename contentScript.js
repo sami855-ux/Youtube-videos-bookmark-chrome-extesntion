7d78e1bcf732d28e9227c539e4bb2116bcf00a6c
sami855-ux <samitale56@outlook.com>
2025-01-02 10:19:18 +0500

    newVideoLoaded function is created also with book mark button

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,28 @@
 ;(() => {
   let youtubeLeftController, youtubePlayer
+  let currentVideo = ""
+
+  //Add a listener to the message from the background script
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, videoId, value } = obj
+
+    if (type === "NEW_VIDEO") {
+      currentVideo = videoId
+
+      //Load the new video
+      newVideoLoaded()
+    }
+  })
+
+  const newVideoLoaded = () => {
+    const bookmarkBntExists = document.getElementsByClassName("bookmark-btn")[0]
+
+    if (!bookmarkBntExists) {
+      const bookmarkBtn = document.createElement("img")
+
+      bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png")
+      bookmarkBnt.className = "ytp-button " + "bookmark-btn"
+      bookmarkBtn.title = "Click to bookmark current timestamp for this video"
+    }
+  }
 })()
